e0c6d8c8c8bb1306eff7e2ba21b4cf3342e53c03
PyaScrapz <postcatalogousados@gmail.com>
2022-09-09 16:44:12 -0500

    Auth

---
+++ src/paginas/DispositivosTitle.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DispositivosTitle = () => {
+  return (
+    <span className='font-bold w-[1440px] text-3xl p-4 ml-20'>Dispositivos</span>
+  )
+}
+
+export default DispositivosTitle
