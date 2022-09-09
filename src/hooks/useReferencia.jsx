db00d063c21582d5c525540b412101857bf74279
PyaScrapz <postcatalogousados@gmail.com>
2022-09-09 17:22:23 -0500

    test

--- src/hooks/useReferencia.jsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react'
-import ReferenciaContext from '../context/ReferenciaProvider'
-
-const useReferencia = () => {
-    return useContext(ReferenciaContext)
-}
-
-export default useReferencia
--- src/paginasAdmin/DispositivosTitleAdmin.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const DispositivosTitle = () => {
-  return (
-    <span className='font-bold w-[1440px] text-3xl p-4 ml-20'>Dispositivos</span>
-  )
-}
-
-export default DispositivosTitle
