6a828a338c3d53b909b04ef42381609eee082ad4
PyaScrapz <postcatalogousados@gmail.com>
2022-09-09 18:17:27 -0500

    Andy post

--- src/paginas/Main.jsx
+++ src/paginas/Main.jsx
@@ -3,19 +3,20 @@ import useReferencia from '../hooks/useReferencia';
 
 const Main = () => {
     const { ref } = useReferencia()
+    console.log(ref)
 
     return (
       <>
-        <div className="shadow rounded-lg flex-row w-[1365px] grid grid-cols-5 text-[22.8px] text-white">
-          {
-            ref.map((reff, index) => 
-              <PreviewReferencia 
-                        key={index}
-                        reff={reff}
-                    />
-            )
-          }
-        </div>
+      <div className="shadow rounded-lg flex-row w-[1365px] grid grid-cols-5 text-[22.8px] text-white">
+        {ref.length ? 
+        ref.map((reff, index) => (
+            <PreviewReferencia 
+                      key={index}
+                      reff={reff}
+                  />
+          ))
+          : <p className=" text-center text-gray-600 uppercase  p-5">No hay Referencias aún</p>}
+      </div>
       </>
   )
 }
