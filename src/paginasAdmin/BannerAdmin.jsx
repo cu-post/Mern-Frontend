4d736149890bab4921fe3eb07578b5c20e0519b9
PyaScrapz <postcatalogousados@gmail.com>
2022-09-09 20:08:17 -0500

    Andy

--- src/paginasAdmin/BannerAdmin.jsx
+++ src/paginasAdmin/BannerAdmin.jsx
@@ -4,7 +4,7 @@ const Banner = () => {
         <div className="bg-[#481373] rounded-[18.91px] w-[1365px] h-[369px]
         mt-[20px] pt-[42px] pr-[231px] pb-[42px] pl-[76px] l-[0%] r-[0%] t-[10.87%] b-[0%] ">
             <div className=" text-white text-[80px] font-semibold">Dales una segunda oportunidad</div>
-            <img className="w-[423px] h-[413px]  l-[66.23%] r-[3.12%] t-[0.24%] b-[0%] ml-[790px] mt-[-326px] " src="../public/chica.png" alt="" />
+            <img className="w-[423px] h-[413px]  l-[66.23%] r-[3.12%] t-[0.24%] b-[0%] ml-[790px] mt-[-326px] " src="../chica.png" alt="" />
         </div>
     </>
   )
