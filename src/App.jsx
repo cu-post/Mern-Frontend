f5df46358375733aa2db08e147af2158e1ac9a67
PyaScrapz <postcatalogousados@gmail.com>
2022-09-09 19:04:47 -0500

    Andy

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,6 @@ import RutaProtegida from './layouts/RutaProtegida'
 
 import Main from './paginas/Main'
 import Proyectos from './paginas/Proyectos'
-import NuevoProyecto from './paginas/NuevoProyecto'
-import Proyecto from './paginas/Proyecto'
-import EditarProyecto from './paginas/EditarProyecto'
-import NuevoColaborador from './paginas/NuevoColaborador'
 
 import {AuthProvider} from './context/AuthProvider'
 import {ProyectosProvider} from './context/ProyectosProvider'
@@ -31,10 +27,6 @@ function App() {
 
                 <Route path="/proyectos" element={<RutaProtegida />}>
                     <Route index element={<Proyectos />} />
-                    <Route path="crear-proyecto" element={<NuevoProyecto />} />
-                    <Route path="nuevo-colaborador/:id" element={<NuevoColaborador />} />
-                    <Route path=":id" element={<Proyecto />} />
-                    <Route path="editar/:id" element={<EditarProyecto />} />
                 </Route>
             </Routes>
           </ProyectosProvider>
