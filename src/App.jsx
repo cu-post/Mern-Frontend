652cb01de76089e9909d327dbe1cb65aa7d0f700
PyaScrapz <postcatalogousados@gmail.com>
2022-09-09 19:36:40 -0500

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import AuthLayout from './layouts/AuthLayout'
+import AuthLayoutMobile from './layouts/AuthLayoutMobile'
 import AuthLayoutAdmin from './layouts/AuthLayoutAdmin'
 //Pagina Principal Usuario
 import Main from './paginas/Main'
+import MainMobile from './paginasAdmin/MainMobile'
 // Login Secreto Admin
 import Login from './paginas/Login'
 import MainAdmin from './paginasAdmin/MainAdmin'
@@ -23,6 +25,11 @@ function App() {
             <Routes>
                 <Route path="/" element={<AuthLayout />}>
                     <Route index element={<Main />} />
+                </Route>
+
+                <Route path="/mobile/:id" element={<AuthLayoutMobile/>}>
+                  <Route index element={<MainMobile />} />            
+                  <Route path="modelo/:id" element={<FormPreviewMobile />} />
                 </Route>
 
                 <Route path="/login" element={<Login />}>
