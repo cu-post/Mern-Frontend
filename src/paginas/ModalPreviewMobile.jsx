c668460b93f4759e9f2c9cd70a8fc581b1977420
PyaScrapz <postcatalogousados@gmail.com>
2022-09-11 15:55:50 -0500

    Update ModalPreviewMobile.jsx

--- src/paginas/ModalPreviewMobile.jsx
+++ src/paginas/ModalPreviewMobile.jsx
@@ -19,8 +19,8 @@ const ModalPreviewMobile = () => {
  
     return (
         <Transition.Root show={ modalPreviewMobile } as={Fragment}>
-            <Dialog as="div" className="fixed z-10 inset-0" onClose={ HandlePreviewMobile }>
-                <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" onClose={ HandlePreviewMobile }>
+                <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
                     <Transition.Child
                         as={Fragment}
                         enter="ease-out duration-300"
@@ -36,7 +36,7 @@ const ModalPreviewMobile = () => {
                     </Transition.Child>
 
                     {/* This element is to trick the browser into centering the modal contents. */}
-                    <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+                    <span className="hidden sm:inline-block lg:inline-table sm:align-middle sm:h-screen lg:h-fit lg:w-fit lg:align-top" aria-hidden="true">
                         &#8203;
                     </span>
 
@@ -49,7 +49,7 @@ const ModalPreviewMobile = () => {
                         leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                         leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                     >
-                            <div className="inline-block align-bottom rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+                            <div className="inline-block lg:inline-table align-bottom rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
 
                             <div className="lg:hidden sm:block absolute top-0 right-0 pt-4 pr-4">
                                 <button
