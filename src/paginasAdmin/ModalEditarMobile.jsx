289d7ca902ca9718a60f82eda03c5e53c0bcd247
PyaScrapz <postcatalogousados@gmail.com>
2022-09-11 14:28:06 -0500

    Update ModalEditarMobile.jsx

--- src/paginasAdmin/ModalEditarMobile.jsx
+++ src/paginasAdmin/ModalEditarMobile.jsx
@@ -19,8 +19,8 @@ const ModalEditarMobile = () => {
  
     return (
         <Transition.Root show={ modalEditarMobile } as={Fragment}>
-            <Dialog as="div" className="fixed inset-0 " onClose={ HandleEditarMobile }>
-                <div className="flex justify-center text-center sm:block sm:p-0">
+            <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" onClose={ HandleEditarMobile }>
+                <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
                     <Transition.Child
                         as={Fragment}
                         enter="ease-out duration-300"
