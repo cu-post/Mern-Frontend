8d186b91a465ef6c1f3fa6a2af91205425ea3aaa
PyaScrapz <postcatalogousados@gmail.com>
2022-09-11 16:42:35 -0500

    Update FormPreviewMobile.jsx

--- src/paginas/FormPreviewMobile.jsx
+++ src/paginas/FormPreviewMobile.jsx
@@ -1,41 +1,32 @@
 import useReferencia from '../hooks/useReferencia';
-import { useEffect, useState } from 'react'
-import clienteAxios from '../config/clienteAxios'
+import { useEffect } from 'react'
 import { useParams } from 'react-router-dom';
 
 const FormPreviewMobile = () => {
 
     const params = useParams();   
-    const [ mobiles, setMobiles] = useState([]);
-    const { ref } = useReferencia();
+    const { ref, LoadMobile, mobile, mobiles } = useReferencia();
 
 
     const mobilefiltrado = mobiles.filter(function(el){
         return (el.code == params.id)
       })
 
-    const number = mobilefiltrado.map(x => x.referenceId)
-
-    const video = mobilefiltrado.map(x => x.video)
+    const id = mobilefiltrado.map(x => x.id)
     
     const reffiltrado = ref.filter(function(el){
-    return (el.id == number)
+    return (el.id == mobile.referenceId)
     })
-      
-      useEffect(() => {
-        const LoadMobiles = async () => {
-            const { data } = await clienteAxios(`/mobile`)
-            setMobiles(data)
-            }
-      
-        LoadMobiles()
-        }, [])
+
+    useEffect(() => {
+        LoadMobile(id)
+    }, [mobiles])
 
     return (
     <>
         <div className='justify-center w-fit lg:w-[884.49px] h-fit lg:h-[706.83px] bg-[#FF5E59] flex flex-col lg:flex-row p-[12px] gap-[24.83px] rounded-[19.27px] items-center'>
             <iframe width="432.89" height="600.25"
-            src={video}
+            src={mobile.video}
             className='my-[11.29px] rounded-[19.27px] gap-[15.41px] w-[310px] h-[400px] md:w-[432.89px] md:h-[660.25px]'
             title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
             allowFullScreen>
@@ -45,7 +36,7 @@ const FormPreviewMobile = () => {
                     {reffiltrado.map(x => x.reference)}
                 </div>
                 <div className='flex justify-center text-[40.63px] text-white'>
-                    Codigo: {mobilefiltrado.map(x => x.code)}
+                    Codigo: {mobile.code}
                 </div>
             </div>
         </div>
