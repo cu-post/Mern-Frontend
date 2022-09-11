149cb1c92909d79abbd3441e6ad85737a6e91706
PyaScrapz <postcatalogousados@gmail.com>
2022-09-10 20:39:33 -0500

    Update ModalPreviewMobile.jsx

--- src/paginas/ModalPreviewMobile.jsx
+++ src/paginas/ModalPreviewMobile.jsx
@@ -47,7 +47,7 @@ const ModalPreviewMobile = () => {
                             <div className="lg:hidden sm:block absolute top-0 right-0 pt-4 pr-4">
                                 <button
                                     type="button"
-                                    className="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                    className="rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                                     onClick={ HandlePreviewMobile }
                                 >
                                 <span className="sr-only">Cerrar</span>
