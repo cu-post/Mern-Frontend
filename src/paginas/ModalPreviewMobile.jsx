b5f7acea75deb5995ab439fb58568b6db89656c1
PyaScrapz <postcatalogousados@gmail.com>
2022-09-11 13:42:42 -0500

    Update ModalPreviewMobile.jsx

--- src/paginas/ModalPreviewMobile.jsx
+++ src/paginas/ModalPreviewMobile.jsx
@@ -19,7 +19,7 @@ const ModalPreviewMobile = () => {
  
     return (
         <Transition.Root show={ modalPreviewMobile } as={Fragment}>
-            <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" onClose={ HandlePreviewMobile }>
+            <Dialog as="div" className="fixed z-10 inset-0" onClose={ HandlePreviewMobile }>
                 <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
                     <Transition.Child
                         as={Fragment}
