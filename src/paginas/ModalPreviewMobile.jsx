ebb3a839be4e381f4a6d293249d1a0f14d43ee10
PyaScrapz <postcatalogousados@gmail.com>
2022-09-10 20:40:47 -0500

    Update ModalPreviewMobile.jsx

--- src/paginas/ModalPreviewMobile.jsx
+++ src/paginas/ModalPreviewMobile.jsx
@@ -6,7 +6,14 @@ import FormPreviewMobileModal from './FormPreviewMobileModal'
 
 const ModalPreviewMobile = () => {
 
-     
+    const refresh = () => {
+        try {                     
+            location.reload();
+        } catch (error) {
+            
+        }
+        
+    }
 
     const {modalPreviewMobile, HandlePreviewMobile} = useReferencia()
  
@@ -42,13 +49,13 @@ const ModalPreviewMobile = () => {
                         leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                         leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                     >
-                            <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+                            <div className="inline-block align-bottom rounded-lg px-4 pt-5 pb-4 text-left transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
 
                             <div className="lg:hidden sm:block absolute top-0 right-0 pt-4 pr-4">
                                 <button
                                     type="button"
-                                    className="rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                                    onClick={ HandlePreviewMobile }
+                                    className="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                    onClick={ refresh }
                                 >
                                 <span className="sr-only">Cerrar</span>
                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
@@ -57,9 +64,9 @@ const ModalPreviewMobile = () => {
                                 </button>
                             </div>
 
-                            <div className="sm:flex sm:items-start">
-                                <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left w-full">
-                                    <Dialog.Title as="h3" className="text-lg leading-6 font-bold text-gray-900">
+                            <div className="sm:flex sm:items-start flex justify-center w-full">
+                                <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                                    <Dialog.Title as="h3" className="text-lg leading-6 font-bold text-gray-900 ">
                                         
                                     </Dialog.Title>
                                     <FormPreviewMobileModal/>
