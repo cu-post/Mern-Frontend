3af96a28b81b36cf38fafd0e18908c4ad0911dfd
PyaScrapz <postcatalogousados@gmail.com>
2022-09-11 13:38:36 -0500

    Update PreviewReferenciaMobileAdmin.jsx

--- src/components/PreviewReferenciaMobileAdmin.jsx
+++ src/components/PreviewReferenciaMobileAdmin.jsx
@@ -19,27 +19,30 @@ const PreviewReferencia = ({mobiles2}) => {
           Editar
         </button>
       </div>
-      <div className="flex justify-center flex-col h-fit w-fit ml-7">
-        <div className='flex justify-center items-center flex-col'>
-        <iframe className="absolute" width="190" height="335">
-        </iframe>
-        <iframe width="180" height="315"
-        src={video}
-        className='mt-6 mb-3 rounded-[24.43px]'
-        title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-        >
-        </iframe>
-        </div>     
-        <button onClick={() => handleModalMostrarMobile(mobiles2)} className='flex-col items-center flex'>
-          <div className='w-fit pl-4 pr-4 pb-4'>
-            {referenceid}
-            <div 
-            className='bg-white w-fit px-4 py-1 mb-6 text-[#8F00FF] font-bold text-[14.9167px] rounded-[22.38px]'>
-                {''}Ver Codigo {code}
+      <div className='items-center justify-center flex'>
+        <div className="flex justify-center flex-col h-fit w-fit">
+          <div className='flex justify-center items-center flex-col'>
+          <iframe className="absolute" width="190" height="335">
+          </iframe>
+          <iframe width="180" height="315"
+          src={video}
+          className='mt-6 mb-3 rounded-[24.43px]'
+          title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+          >
+          </iframe>
+          </div>     
+          <button onClick={() => handleModalMostrarMobile(mobiles2)} className='flex-col items-center flex'>
+            <div className='w-fit pl-4 pr-4 pb-4'>
+              {referenceid}
+              <div 
+              className='bg-white w-fit px-4 py-1 mb-6 text-[#8F00FF] font-bold text-[14.9167px] rounded-[22.38px]'>
+                  {''}Ver Codigo {code}
+              </div>
             </div>
-          </div>
-        </button>   
-      </div>         
+          </button>   
+        </div>         
+      </div>
+      
     </div>    
     <ModalPreviewMobile/>
     <ModalEditarMobileAdmin/>
