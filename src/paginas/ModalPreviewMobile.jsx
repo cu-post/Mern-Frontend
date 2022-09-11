fb21d715118944ef4d60d9c977c0f525bd2de6ac
PyaScrapz <postcatalogousados@gmail.com>
2022-09-10 20:32:51 -0500

    Update ModalPreviewMobile.jsx

--- src/paginas/ModalPreviewMobile.jsx
+++ src/paginas/ModalPreviewMobile.jsx
@@ -66,7 +66,7 @@ const ModalPreviewMobile = () => {
 
                             <div className="sm:flex sm:items-start flex justify-center w-full overflow-y-auto">
                                 <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                                    <Dialog.Title as="h3" className="text-lg leading-6 font-bold text-gray-900 ">
+                                    <Dialog.Title as="h3" className="text-lg leading-6 font-bold text-gray-900 overflow-y-auto">
                                         
                                     </Dialog.Title>
                                     <FormPreviewMobileModal/>
