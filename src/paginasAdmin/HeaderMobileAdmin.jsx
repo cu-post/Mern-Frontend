f9c80e0455fd2e80feea21509f70365bac28c6a9
Andy Nya <zodiakapp@spank-art.co>
2022-09-12 03:30:51 -0500

    PlaceHolders

--- src/paginasAdmin/HeaderMobileAdmin.jsx
+++ src/paginasAdmin/HeaderMobileAdmin.jsx
@@ -38,7 +38,7 @@ const HeaderMobileAdmin = () => {
           <input
           className="bg-[#FF5E59] placeholder-white pt-[16.0377px] pb-[16.0377px] pl-[16.0377px] gap-[16.04px]
           w-[300px] md:w-[350.13px] lg:w-[396.13px] h-[59.08px] rounded-[8.01887px] text-white"
-          placeholder="Codigo del telefono"
+          placeholder="Codigo"
           type="text"
           value={busqueda}
           onChange={e => setBusqueda(e.target.value)}
