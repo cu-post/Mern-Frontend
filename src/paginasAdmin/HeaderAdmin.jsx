70ce221162f3430caac45e8e956a3a046470f813
PyaScrapz <postcatalogousados@gmail.com>
2022-09-15 11:22:20 -0500

    Search and icon Fulll

--- src/paginasAdmin/HeaderAdmin.jsx
+++ src/paginasAdmin/HeaderAdmin.jsx
@@ -25,23 +25,26 @@ const HeaderAdmin = () => {
         <div className="text-[30px] md:text-[35px] lg:text-[43.58px] flex-shrink-0 font-semibold text-[#481373]">Celulares Usados</div>
     </div>
       <div className="flex flex-col justify-center items-center md:justify-between md:flex-row gap-[5px] w-0/2 h-fit mt-[2.84px]">
-          <div className='flex flex-row items-center'>
+          <div className='flex flex-row relative'>
           <button
           onClick={handleCerrarSesion}
           className='p-3 ml-4 lg:ml-0 rounded-[9.56px] h-[57.73584747314453px] w-[57.73584747314453px] bg-[#FF5E59]'
           >
               <img src="../exit_to_app.png" alt="" />
           </button>
-          <input
-          className="bg-[#FF5E59] placeholder-white pt-[16.0377px] pb-[16.0377px] pl-[16.0377px] gap-[16.04px]
-          w-[240px] ml-2 sm:w-[300.13px] lg:w-[350px] xl:w-[470px] 2xl:w-[340px] sm:ml-2 h-[h-[57.73584747314453px]] rounded-[8.01887px] text-white"
+          <div class="flex absolute inset-y-0 left-20 md:left-20 lg:left-16 xl:left-16 2xl:left-16 items-center pl-3 pointer-events-none">
+              <svg aria-hidden="true" class="w-5 h-5 text-white dark:text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+          </div>
+          <input type="search" id="default-search"
+          className="block pl-10 bg-[#FF5E59] placeholder-white pt-[16.0377px] pb-[16.0377px] gap-[16.04px] border border-gray-300 pr-5
+          w-[230px] ml-2 sm:w-[300.13px] lg:w-[350px] xl:w-[470px] 2xl:w-[340px] sm:ml-2 h-[h-[57.73584747314453px]] rounded-[8.01887px] text-white"
           placeholder="Buscar Modelo"
-          type="text"
           value={busqueda}
           onChange={e => setBusqueda(e.target.value)}
-          />
+          />          
           {usuariosFiltrados.length > 0 && (
-            <div className="flex-col absolute mt-14 md:mt-0 sm:top-16">
+            <div className="flex-col absolute mt-14 md:mt-0 sm:top-16 ml-20 lg:ml-16">
                 {usuariosFiltrados.map( ref => (
                     <a
                         key={ref.id}
